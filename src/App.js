21e736bdbef09da09c432917b91f9e3b1a310e03
David Ridgley <Arximiro@outlook.com>
2018-03-30 23:09:40 -0400

    removed superfluous files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { View, Text } from 'react-native';
 class App extends Component {
   render() {
     return (
-      <Provider createStore={createStore()}>
+      <Provider createStore={createStore(() => {})}>
         <View>
           <Text>
             Hello from React Native!
